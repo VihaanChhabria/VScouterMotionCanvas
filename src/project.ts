17a21b93627a60019a9f794111a8d97035b22b27
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-11-25 20:45:13 -0500

    changed default file name

--- src/project.ts
+++ src/project.ts
@@ -1,7 +1,7 @@
 import {makeProject} from '@motion-canvas/core';
 
-import example from './scenes/example?scene';
+import slideshow from './scenes/slideshow?scene';
 
 export default makeProject({
-  scenes: [example],
+  scenes: [slideshow],
 });
