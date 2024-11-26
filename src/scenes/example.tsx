e8c8fe6f88d654d7707b1cdc198b2f11d59dc9da
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-11-25 20:38:39 -0500

    changed color and color variable names aswell as added conclusion slide

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -20,14 +20,18 @@ import {
 import VScouterLogoImage from "../media/VScouterLogo.png";
 import PowerBIImage from "../media/PowerBIImage.svg";
 import FRCFieldImage from "../media/FRCField.png";
+import VihaanLinkTreeImage from "../media/VihaanLinkTree.png";
 
 export default makeScene2D(function* (view) {
-  var green = "B2F2BB";
-  var yellow = "FFEC99";
-  var blue = "A5D8FF";
+  // var green = "B8336A";
+  // var yellow = "6893AC";
+  // var blue = "F3CA40";
+  var colorOne = "FFD6CC";
+  var colorBackground = "FFF3B0";
+  var colorTwo = "CFEAFF";
 
   const background = createRef<Rect>();
-  view.add(generateRect(1920, 1080, yellow, background, 0, 0, [], 0));
+  view.add(generateRect(1920, 1080, colorBackground, background, 0, 0, [], 0));
 
   const presentationIntro = createRef<Layout>();
   view.add(
@@ -68,7 +72,7 @@ export default makeScene2D(function* (view) {
           ? generateRect(
               100,
               100,
-              blue,
+              colorTwo,
               alliance1,
               coord[0],
               coord[1],
@@ -78,7 +82,7 @@ export default makeScene2D(function* (view) {
           : generateRect(
               100,
               100,
-              blue,
+              colorTwo,
               undefined,
               coord[0],
               coord[1],
@@ -98,18 +102,27 @@ export default makeScene2D(function* (view) {
           ? generateRect(
               80,
               80,
-              green,
+              colorOne,
               alliance1Pick,
               0 - 115 * index,
               440,
               [],
               20
             )
-          : generateRect(80, 80, green, undefined, 0 - 115 * index, 440, [], 20)
+          : generateRect(
+              80,
+              80,
+              colorOne,
+              undefined,
+              0 - 115 * index,
+              440,
+              [],
+              20
+            )
       )}
       ,
       {[...Array(8)].map((_, index) =>
-        generateRect(80, 80, green, undefined, 0 + 115 * index, 440, [], 20)
+        generateRect(80, 80, colorOne, undefined, 0 + 115 * index, 440, [], 20)
       )}
     </Layout>
   );
@@ -137,9 +150,15 @@ export default makeScene2D(function* (view) {
 
   const scoutingMessage = createRef<Rect>();
   view.add(
-    generateRect(1204.93, 407.5, green, scoutingMessage, 0, -452.65 - 407.5, [
-      generateText("Scouting!", 100),
-    ])
+    generateRect(
+      1204.93,
+      407.5,
+      colorOne,
+      scoutingMessage,
+      0,
+      -452.65 - 407.5,
+      [generateText("Scouting!", 100)]
+    )
   );
 
   yield* beginSlide("Scouting Message");
@@ -156,22 +175,22 @@ export default makeScene2D(function* (view) {
 
   const scoutingFlowChart = createRef<Rect>();
   view.add(
-    generateRect(1845.4, 600.14, green, scoutingFlowChart, 0, 501 + 600.14, [
-      generateRect(527.07, 356.81, blue, undefined, -613.62),
+    generateRect(1845.4, 600.14, colorOne, scoutingFlowChart, 0, 501 + 600.14, [
+      generateRect(527.07, 356.81, colorTwo, undefined, -613.62),
       generateText("Data Collection", 40, -613.62, 0),
       generateArrow([
         [-613.62 + 527.07 / 2, 0],
         [0 - 527.07 / 2, 0],
       ]),
 
-      generateRect(527.07, 356.81, blue, undefined, 0),
+      generateRect(527.07, 356.81, colorTwo, undefined, 0),
       generateText("Parsing", 40, 0, 0),
       generateArrow([
         [0 + 527.07 / 2, 0],
         [613.62 - 527.07 / 2, 0],
       ]),
 
-      generateRect(527.07, 356.81, blue, undefined, 613.62),
+      generateRect(527.07, 356.81, colorTwo, undefined, 613.62),
       generateText("Analysis", 40, 613.62, 0),
       generateArrow([
         [613.62, 89.76 * 2],
@@ -229,14 +248,22 @@ export default makeScene2D(function* (view) {
 
   const vscouterDescription = createRef<Rect>();
   view.add(
-    generateRect(872.5, 814.87, blue, vscouterDescription, 960.77 + 440.78, 0, [
-      generateText(
-        `- Data collection though a website found at \nhttps://vscouter.netlify.app/\n\n - Scouting can be done offline in \nenvironments with no internet\n\n- Auto completion to make selecting teams \nto scout easier\n\n- USB flash drive data collection process`,
-        43,
-        0,
-        0
-      ),
-    ])
+    generateRect(
+      872.5,
+      814.87,
+      colorTwo,
+      vscouterDescription,
+      960.77 + 440.78,
+      0,
+      [
+        generateText(
+          `- Data collection though a website found at \nhttps://vscouter.netlify.app/\n\n - Scouting can be done offline in \nenvironments with no internet\n\n- Auto completion to make selecting teams \nto scout easier\n\n- USB flash drive data collection process`,
+          43,
+          0,
+          0
+        ),
+      ]
+    )
   );
 
   yield* beginSlide("VScouter Explanation");
@@ -251,9 +278,15 @@ export default makeScene2D(function* (view) {
 
   const dataCollectionDemo = createRef<Rect>();
   view.add(
-    generateRect(825, 377.76, blue, dataCollectionDemo, 0, -654.07 - 377.76, [
-      generateText("Data Collection Demo", 75),
-    ])
+    generateRect(
+      825,
+      377.76,
+      colorTwo,
+      dataCollectionDemo,
+      0,
+      -654.07 - 377.76,
+      [generateText("Data Collection Demo", 75)]
+    )
   );
 
   yield* beginSlide("Data Collection Demo");
@@ -268,13 +301,13 @@ export default makeScene2D(function* (view) {
   const leftParsingFlowChart = createRef<Layout>();
   view.add(
     <Layout ref={leftParsingFlowChart} x={-979.95}>
-      {generateRect(380, 200, green, undefined, -669.95, -301.92, [
+      {generateRect(380, 200, colorOne, undefined, -669.95, -301.92, [
         generateText("Red Scouter 1", 45),
       ])}
-      {generateRect(380, 200, green, undefined, -669.95, 0, [
+      {generateRect(380, 200, colorOne, undefined, -669.95, 0, [
         generateText("Red Scouter 2", 45),
       ])}
-      {generateRect(380, 200, green, undefined, -669.95, 301.92, [
+      {generateRect(380, 200, colorOne, undefined, -669.95, 301.92, [
         generateText("Red Scouter 3", 45),
       ])}
       {generateArrow([
@@ -295,13 +328,13 @@ export default makeScene2D(function* (view) {
   const rightParsingFlowChart = createRef<Layout>();
   view.add(
     <Layout ref={rightParsingFlowChart} x={979.95}>
-      {generateRect(380, 200, green, undefined, 669.95, -301.92, [
+      {generateRect(380, 200, colorOne, undefined, 669.95, -301.92, [
         generateText("Blue Scouter 1", 45),
       ])}
-      {generateRect(380, 200, green, undefined, 669.95, 0, [
+      {generateRect(380, 200, colorOne, undefined, 669.95, 0, [
         generateText("Blue Scouter 2", 45),
       ])}
-      {generateRect(380, 200, green, undefined, 669.95, 301.92, [
+      {generateRect(380, 200, colorOne, undefined, 669.95, 301.92, [
         generateText("Blue Scouter 3", 45),
       ])}
       {generateArrow([
@@ -321,9 +354,15 @@ export default makeScene2D(function* (view) {
 
   const centerParsingFlowChart = createRef<Rect>();
   view.add(
-    generateRect(455, 420, blue, centerParsingFlowChart, 0, -541.46 - 420 / 2, [
-      generateText("Server", 65),
-    ])
+    generateRect(
+      455,
+      420,
+      colorTwo,
+      centerParsingFlowChart,
+      0,
+      -541.46 - 420 / 2,
+      [generateText("Server", 65)]
+    )
   );
 
   yield* beginSlide("Parsing Flowchart");
@@ -343,8 +382,8 @@ export default makeScene2D(function* (view) {
       {generateTypeOfTransfer(
         -615,
         75,
-        blue,
-        green,
+        colorTwo,
+        colorOne,
         "Bluetooth",
         "- Offline Data Sharing\n- Contactless",
         "- Small Max Bandwidth\n- Device Pairing Challenges\n- Compatibility Limitations"
@@ -352,8 +391,8 @@ export default makeScene2D(function* (view) {
       {generateTypeOfTransfer(
         0,
         75,
-        blue,
-        green,
+        colorTwo,
+        colorOne,
         "Flash Drives",
         "- Offline Data Sharing\n- Large Bandwidth\n- Reliable\n- Universally Compatible",
         "- Manual Process\n- Lack of Automation\n- No Real-Time Updates",
@@ -362,8 +401,8 @@ export default makeScene2D(function* (view) {
       {generateTypeOfTransfer(
         615,
         75,
-        blue,
-        green,
+        colorTwo,
+        colorOne,
         "QR Codes",
         "- Offline Data Sharing\n- Quick Data Sharing\n- Universally Compatible\n- Cost-Effective",
         "- Smaller Max Bandwidth\n- Manual Process\n- No Real-Time Updates\n- Possibility of Data Splits",
@@ -385,7 +424,7 @@ export default makeScene2D(function* (view) {
 
   const parseDataDemo = createRef<Rect>();
   view.add(
-    generateRect(825, 377.76, blue, parseDataDemo, 0, -654.07 - 377.76, [
+    generateRect(825, 377.76, colorTwo, parseDataDemo, 0, -654.07 - 377.76, [
       generateText("Parsing Data Demo", 75),
     ])
   );
@@ -400,7 +439,7 @@ export default makeScene2D(function* (view) {
   const analysisToPowerBI = createRef<Layout>();
   view.add(
     <Layout ref={analysisToPowerBI} x={-959.58 - 1568.5 / 2}>
-      {generateRect(605.18, 301.1, blue, undefined, -447.08, 0, [
+      {generateRect(605.18, 301.1, colorTwo, undefined, -447.08, 0, [
         generateText("Analysis", 75),
       ])}
       {generateArrow([
@@ -432,14 +471,22 @@ export default makeScene2D(function* (view) {
 
   const powerBIDescription = createRef<Rect>();
   view.add(
-    generateRect(872.5, 814.87, blue, powerBIDescription, 960.77 + 440.78, 0, [
-      generateText(
-        `- An analytics tool developed by Microsoft \nthat enables users to visualize data and \ncreate interactive reports.\n\n- Has the ability to import data from \nsources like databases, Excel, and CSV files\n\n- Free for personal use`,
-        43,
-        0,
-        0
-      ),
-    ])
+    generateRect(
+      872.5,
+      814.87,
+      colorTwo,
+      powerBIDescription,
+      960.77 + 440.78,
+      0,
+      [
+        generateText(
+          `- An analytics tool developed by Microsoft \nthat enables users to visualize data and \ncreate interactive reports.\n\n- Has the ability to import data from \nsources like databases, Excel, and CSV files\n\n- Free for personal use`,
+          43,
+          0,
+          0
+        ),
+      ]
+    )
   );
 
   yield* beginSlide("Analysis P2");
@@ -453,7 +500,7 @@ export default makeScene2D(function* (view) {
   const analysisDemoBox = createRef<Rect>();
   const analysisDemoText = createRef<Txt>();
   view.add(
-    generateRect(825, 377.76, blue, analysisDemoBox, 0, -654.07 - 377.76, [
+    generateRect(825, 377.76, colorTwo, analysisDemoBox, 0, -654.07 - 377.76, [
       generateText("Analysis Demo", 75, 0, 0, "black", analysisDemoText),
     ])
   );
@@ -467,15 +514,49 @@ export default makeScene2D(function* (view) {
   powerBIImageRef().remove();
   powerBIDescription().remove();
 
+  const conclusionPage = createRef<Layout>();
+  view.add(
+    <Layout ref={conclusionPage} x={960+(1684.05/2)}>
+      {generateText("Thank You!", 150, -420, -325.18)}
+      <Line
+        stroke={"black"}
+        lineWidth={10}
+        points={[
+          [-787.84 - 31.14, -256.43],
+          [9.43 - 31.14, -256.43],
+        ]}
+      />
+
+      {generateText("Contact Me Or", 60, -420, -171.2)}
+      {generateText("Find The Code For VScouter!", 60, -420, -100)}
+      {generateArrow([
+        [-420, -70],
+        [-420, 50],
+      ])}
+      <Img src={VihaanLinkTreeImage} width={433.17} height={433.17} x={-420} y={275}/>
+
+      <Img
+        src={VScouterLogoImage}
+        width={814.62}
+        height={814.62}
+        x={484.26}
+        y={0}
+      />
+    </Layout>
+  );
+
   yield* beginSlide("Conclusion");
   yield* all(
     analysisDemoText().y(-1000, 1),
     analysisDemoBox().width(1920, 1),
     analysisDemoBox().height(1080, 1),
     analysisDemoBox().radius(0, 1),
-    analysisDemoBox().lineWidth(0, 1)
-  )
+    analysisDemoBox().lineWidth(0, 1),
+    conclusionPage().x(0, 1)
+  );
   analysisDemoText().remove();
+
+  yield* beginSlide("-");
 });
 
 function generateRect(
